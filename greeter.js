8f1cdbc1407945e5f225bc2b8d4a7c0af2caebe8
Anastasia Koschel <anastasia_koschel@teilnehmer.cdemy.com>
2023-09-11 11:20:22 +0200

    1.1

--- greeter.js
+++ greeter.js
@@ -1,41 +1,72 @@
+//Funktion Liste
+window.onload= function() {
+    main();
+    
+}
+// Global Variablen
+let männlicheGeschlecht;
+let weiblicheGeschlecht;
+let inputButton = document.getElementById('inputButton');
+let vorname = document.getElementById('vornameId');
+let nachname = document.getElementById('nachnameId');
+let geschlecht = document.getElementById('geschlechtId');
+let alter = document.getElementById('alterId');
+
 // verbunden mit Css und HTML
-// Funktion main*()
-// ziet den eingegebene Alter, pruft ob in Feld mit Alter die zahl eingegeben wurde, falls nicht wird Fehler gemeldet
-// "Schreiben Sie wie Alt sind Sie mit Zahlen"
-// Nutzer wird verpflichtet Alter neu zu schreiben
-// ziet eingegebene Geschlecht und vergleicht es mit ein Array von Geschlechts
-// es werden zwei Array sein mit verschiedene Geschlechte weiblicheGeschlecht[] und männlicheGeschlecht[] 
-// Fals Prufungs Schleife die durch zwei Array geht keine Zufall in Arreys findet wird ein Fehler gemeldet
-// "Es sieht so aus, dass Sie sich vertippt haben. Sie können Ihre Geschlecht als Buchstabe m oder w schreiben. 
-// Oder schreiben Sie ein ganzes Wort ohne Tippfehler"
-// Wenn alles ok. Die Seite wird Neu gerendert
-// Auf die seite wird Stehen 
-//  Falls Person Junger als 40 schreibt
-// "Hi, *User Vorname*!
-// Du bist *User Alter*
-// Willst du ein Spiel spielen"
-// Falls Person älter als 40 und ein Mann ist. Schreibt:
-// "Guten Tag, Herr *User Nachname*
-// Sie siend *User Alter*
-// Wollen Sie Ein Spiel spielen?"
-// Falls Person älter als 40 und ein Frau ist. Schreibt:
-// "Guten Tag, Frau *User Nachname*
-// Sie sind *User Alter*
-// Wollen Sie Ein Spiel spielen?"
-// Feld mit Ja und Nein Bottoms.
-// Falls Ja wird Seite neu geladen 
-// die Regeln werden Erklärt
-// Regel Erklärung für Person unter 40:
-// "Die Regeln sind einfach. Ich würfle ein Zahl ab 0 bis 100 und du soll raten welche.
-// Du hast nur 6 Versuche um Zahl zu erraten. Nach jeden dein Versuch werde ich dir sagen ob mein Zahl größer oder kleiner.
-// Wenn alles klar, dann möge der Macht mit dir sein. Wenn nicht, dann hast du Pech, niemand kann mir jetzt Stopen ;)
-// Es geht los. Ich hab ein Zahl. Rate welche"
-// Regeln Erklärung für Person über oder gleich 40:
-// "Die Regeln sind einfach. Ich würfle ein Zahl und Sie sollen raten welche.
-// Sie haben nur 6 fersuche um Zahl zu erraten. Nach jeden Ihre Versuch werde ich ihnen sagen, ob mein Zahl größer oder kleiner.
-// Möge der Macht mit ihnen sein.
-// Es geht los. Ich hab ein Zahl. Raten Sie welche"
-// Und dann startet spiel()
+// inputButton.addEventListener("click", function (){
+function main(){
+    
+
+    inputValVorname.addEventListener("click", function(){
+        let inputValVorname = vorname.value;
+        let inputValNachname = nachname.value;
+        let inputValGeschlecht = geschlecht.value;
+        let inputValAlter = alter.value;
+        
+        
+        alert(inputValAlter);
+        alert(inputValVorname);
+        alert(inputValNachname);
+        alert(inputValGeschlecht);
+});
+}
+    // ziet den eingegebene Alter, pruft ob in Feld mit Alter die zahl eingegeben wurde, falls nicht wird Fehler gemeldet
+    // "Schreiben Sie wie Alt sind Sie mit Zahlen"
+    // Nutzer wird verpflichtet Alter neu zu schreiben
+    // ziet eingegebene Geschlecht und vergleicht es mit ein Array von Geschlechts
+    // es werden zwei Array sein mit verschiedene Geschlechte weiblicheGeschlecht[] und männlicheGeschlecht[] 
+    // Fals Prufungs Schleife die durch zwei Array geht keine Zufall in Arreys findet wird ein Fehler gemeldet
+    // "Es sieht so aus, dass Sie sich vertippt haben. Sie können Ihre Geschlecht als Buchstabe m oder w schreiben. 
+    // Oder schreiben Sie ein ganzes Wort ohne Tippfehler"
+    // Wenn alles ok. Die Seite wird Neu gerendert
+    // Auf die seite wird Stehen 
+    //  Falls Person Junger als 40 schreibt
+    // "Hi, *User Vorname*!
+    // Du bist *User Alter*
+    // Willst du ein Spiel spielen"
+    // Falls Person älter als 40 und ein Mann ist. Schreibt:
+    // "Guten Tag, Herr *User Nachname*
+    // Sie siend *User Alter*
+    // Wollen Sie Ein Spiel spielen?"
+    // Falls Person älter als 40 und ein Frau ist. Schreibt:
+    // "Guten Tag, Frau *User Nachname*
+    // Sie sind *User Alter*
+    // Wollen Sie Ein Spiel spielen?"
+    // Feld mit Ja und Nein Bottoms.
+    // Falls Ja wird Seite neu geladen 
+    // die Regeln werden Erklärt
+    // Regel Erklärung für Person unter 40:
+    // "Die Regeln sind einfach. Ich würfle ein Zahl ab 0 bis 100 und du soll raten welche.
+    // Du hast nur 6 Versuche um Zahl zu erraten. Nach jeden dein Versuch werde ich dir sagen ob mein Zahl größer oder kleiner.
+    // Wenn alles klar, dann möge der Macht mit dir sein. Wenn nicht, dann hast du Pech, niemand kann mir jetzt Stopen ;)
+    // Es geht los. Ich hab ein Zahl. Rate welche"
+    // Regeln Erklärung für Person über oder gleich 40:
+    // "Die Regeln sind einfach. Ich würfle ein Zahl und Sie sollen raten welche.
+    // Sie haben nur 6 fersuche um Zahl zu erraten. Nach jeden Ihre Versuch werde ich ihnen sagen, ob mein Zahl größer oder kleiner.
+    // Möge der Macht mit ihnen sein.
+    // Es geht los. Ich hab ein Zahl. Raten Sie welche"
+    // Und dann startet spiel()
+
 // Wie funktioniert Spiel Funktion:
 // Ein random Zahl ab 0 bis 100 wird "gewürfelt"
 // Nutzer gibt ein Zahl die Nutzerzahl wird mit Randomzahl wergleicht 
